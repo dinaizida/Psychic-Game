a04a5864e726ed43bd2d7bb43f28b39a1321daa9
Dina <>
2018-04-15 00:22:27 -0500

    game done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,39 @@
-var computerChoices = ["a", "b", "c", "d", "e"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
-var userGuess = "";
-var numberGames = 10;
 
+var userGuessArray = [];
+var numberGames = 10;
 
 document.onkeyup = function(event) {
 
     var userGuess = event.key.toLowerCase();
-
+    var userGuessOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if (userGuess === computerGuess) {
-        wins++;
-        // userGuess = userGuess.push(userGuess);
-
-    } else {
-        losses++;
-        numberGames--;
-        guessesLeft = numberGames;
-
-        console.log(numberGames);
-        if (numberGames === 0) {
-            
-            var gameover = "<h1> Game over, refresh your browser to start again</h1>";
-
-        document.querySelector("#gameover").innerHTML = gameover;
-            wins = 0;
-            losses = 0;
-            guessesLeft = 0;
-            userGuess = "";
-            numberGames = 10;
-
+    if (userGuessOptions.indexOf(userGuess) > -1) {
+        if (userGuess === computerGuess) {
+            wins++;
+            userGuessArray.push(userGuess);
+        } else {
+            losses++;
+            numberGames--;
+            userGuessArray.push(userGuess);
+            console.log(userGuessArray);
+            console.log(numberGames);
+            if (numberGames === 0) {
+                var gameover = "<h3> Game is over! Refresh you browser to start again.</h3>";
+                document.querySelector("#gameover").innerHTML = gameover;
+                
+            }
         }
-
     }
-
     var html =
         "<p> Winns : " + wins + "</p>" +
         "<p> Looses : " + losses + "</p>" +
-        "<p> Guesses Left : " + guessesLeft + "</p>" +
-        "<p> Your Guesses so Far : " + userGuess + "</p>";
+        "<p> Guesses Left : " + numberGames + "</p>" +
+        "<p> Your Guesses so Far : " + userGuessArray + "   " + "</p>";
 
     document.querySelector("#game").innerHTML = html;
-
 };
+
